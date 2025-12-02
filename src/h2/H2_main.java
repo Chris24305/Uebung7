eb0279aca9f78518364e828af5997d9c95406935
Chris24305 <christian.schaa@web.de>
2025-12-02 19:30:45 +0100

    H2V3

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 
 public class H2_main {
 	public static void main(String[] args) {
-		int[] test1 = { 5, 3, 7, 3, 4, 7, 8 };
+	/*	int[] test1 = { 5, 3, 7, 3, 4, 7, 8 };
 		int[] test2 = { 5, 3, 7, 3, 4, 7, 8 };
 		int test3 = 1;
 		int test4 = 6;
 		int[] result = change(test1, test2, test3, test4);
-		for (int i= 0; i<result.length; i++) {
-			System.out.print (result[i]+" ");	
-		} 
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + " ");
+		}	*/
 	}
 
 	public static int[] change(int[] a, int[] b, int start, int end) {
